9c8598b29bcde346e9e17346f4659ea79f35c18b
rplantefeve <romain.plantefeve@gmail.com>
2017-07-05 12:04:50 +0200

    Ajout Concert classe association et chef d'orchestre

--- src/main/java/edu/formation/orchestre/MainInstrument.java
+++ src/main/java/edu/formation/orchestre/MainInstrument.java
@@ -3,6 +3,13 @@
  */
 package edu.formation.orchestre;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import edu.formation.heritage.Personne;
+
 /**
  * Classe principale de l'application de gestion des instruments
  * 
@@ -15,8 +22,9 @@ public class MainInstrument
      * Méthode principale (point d'entrée)
      * 
      * @param args
+     * @throws ParseException 
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws ParseException
     {
         System.out.println(IJouable.FOO);
         
@@ -24,9 +32,8 @@ public class MainInstrument
 
         
         // on rentre le nom et le prix de chaque instrument
-        Instrument inst1 = new InstrumentACordes("guitare", 1500.0f);
-        Instrument inst2 = new InstrumentAVent("batterie", 9999.99f);
-        
+        Instrument guitare = new Instrument("guitare", 1500.0f);
+        Instrument batterie = new Instrument("batterie", 9999.99f);
         System.out.println("Nombre d'instruments : " + Instrument.nombreInstruments);
 
 
@@ -38,21 +45,34 @@ public class MainInstrument
         saxophone.depreciation(0.5f);
 
         // on affiche les informations de chaque instrument
-        inst1.afficher();
-        inst2.afficher();
+        guitare.afficher();
+        batterie.afficher();
         // on baisse le prix de la guitare
-        inst1.depreciation();
+        guitare.depreciation();
         // on affiche le nouveau prix de la guitare
         System.out.println(
-                "Nouveau prix de la guitare : " + inst1.getPrix() + " euros.");
+                "Nouveau prix de la guitare : " + guitare.getPrix() + " euros.");
         
-        Orchestre o1 = new Orchestre("La fanfaronnerie", TypeOrchestre.harmonique);
-        o1.ajouterInstrument(inst1);
-        o1.ajouterInstrument(inst2);
-        o1.ajouterInstrument(cordes1);
-        o1.ajouterInstrument(saxophone);
+        Orchestre concertoOpera = new Orchestre("La fanfaronnerie", TypeOrchestre.harmonique);
+        concertoOpera.ajouterInstrument(guitare);
+        concertoOpera.ajouterInstrument(batterie);
+        concertoOpera.ajouterInstrument(cordes1);
+        concertoOpera.ajouterInstrument(saxophone);
         
         
-        o1.afficherOrchestre();
+        concertoOpera.afficherOrchestre();
+        
+        // j'instancie le musicien
+        Musicien brel = new Musicien("Jacques" ,"Brel", "triste");
+        // le musicien Jacquo est le chef d'orchestre
+        concertoOpera.setChef(brel);
+        
+        // je créé la liste des instruments dont il a joué
+        ArrayList<Instrument> listeInstru = new ArrayList<>();
+        listeInstru.add(guitare);
+        listeInstru.add(batterie);
+        // j'ajoute le concert auquel il a participé
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        brel.ajouterConcert("Opéra de Paris", simpleDateFormat.parse("31-12-2017"), listeInstru);
     }
 }
